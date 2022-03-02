bbd59fe0a90417abc1c3ed90bb528ea2634fbc96
edsonllima <focusti@hotmail.com>
2022-03-02 10:12:04 -0300

    organizando codigo

---
+++ src/js/codificador.js
@@ -0,0 +1,16 @@
+/* Regras Codificador: 
+"e" é convertido para "enter" 
+"i" é convertido para "imes"
+"a" é convertido para "ai"
+"o" é convertido para "ober"
+"u" é convertido para "ufat"
+Apenas letras minúsculas
+Não permite acentuação   
+*/
+
+let text = document.getElementById("input-texto");
+let textModify;
+
+function letterReplace(text) {
+  return;
+}
--- src/js/decodificador.js
+++ src/js/decodificador.js
@@ -1,14 +1,3 @@
-
-/* Regras Codificador: 
-"e" é convertido para "enter" 
-"i" é convertido para "imes"
-"a" é convertido para "ai"
-"o" é convertido para "ober"
-"u" é convertido para "ufat"
-Apenas letras minúsculas
-Não permite acentuação   
-*/
-
 /* Regras Decodificador: 
 "enter" é convertido para "e" 
 "imes" é convertido para "i"
