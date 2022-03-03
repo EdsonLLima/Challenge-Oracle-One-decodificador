abcb6093dccfcdbc2675d3843d9fead4d09e3212
edsonllima <megadev@DESKTOP-GCF2849.localdomain>
2022-03-03 01:31:55 -0300

    atualizado botao cripto grafar

--- src/js/codificador.js
+++ src/js/codificador.js
@@ -1,3 +1,44 @@
+let inputText = document.getElementById("input-text");
+inputText.focus();
+// inputText.value = "heroi";
+let msgEncript = document.getElementById("msg");
+// console.log((msgEncript.value = inputText.value));
+
+let buttonEncrypt = document.getElementById("button-encrypt");
+
+buttonEncrypt.addEventListener("click", (event) => {
+  event.preventDefault();
+  letterCode();
+});
+
+let letterCode = () => {
+  //recebe conteudo do input e convert as vogais
+  let getInputText = inputText.value
+    .replace(/[e\é\ê]/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  msgEncript.value = getInputText; //add conteudo no input msg
+};
+// letterCode();
+// let letterDecoding = () => {
+//   let inputText = document.getElementById("input-text").value;
+//   //recebe conteudo do input e convert as vogais
+//   let getInputText = inputText
+//     .replace(/enter/gi, "e")
+//     .replace(/imes/gi, "i")
+//     .replace(/ai/gi, "a")
+//     .replace(/ober/gi, "o")
+//     .replace(/ufat/gi, "u");
+
+//   let msgEncript = document.getElementById("msg");
+//   msgEncript.value = getInputText; //add conteudo no input msg
+
+//   return console.log(msgEncript.value);
+// };
+
 /* Regras Codificador: 
 "e" é convertido para "enter" 
 "i" é convertido para "imes"
@@ -7,10 +48,3 @@
 Apenas letras minúsculas
 Não permite acentuação   
 */
-
-let text = document.getElementById("input-texto");
-let textModify;
-
-function letterReplace(text) {
-  return;
-}
