f119b7840f9940f46727584076443d9888c22a80
edsonllima <focusti@hotmail.com>
2022-03-04 08:16:03 -0300

    feito criptografia - craindo descriptografia

--- src/js/codificador.js
+++ src/js/codificador.js
@@ -1,17 +1,17 @@
 let inputText = document.getElementById("input-text");
 inputText.focus();
-// inputText.value = "heroi";
+
 let msgEncript = document.getElementById("msg");
-// console.log((msgEncript.value = inputText.value));
 
 let buttonEncrypt = document.getElementById("button-encrypt");
 
 buttonEncrypt.addEventListener("click", (event) => {
   event.preventDefault();
-  letterCode();
+  encryptLetters(); //Criptografa as letras
+  cleaningInput();
 });
 
-let letterCode = () => {
+let encryptLetters = () => {
   //recebe conteudo do input e convert as vogais
   let getInputText = inputText.value
     .replace(/[e\é\ê]/gi, "enter")
@@ -20,24 +20,13 @@ let letterCode = () => {
     .replace(/o/gi, "ober")
     .replace(/u/gi, "ufat");
 
-  msgEncript.value = getInputText; //add conteudo no input msg
+  msgEncript.value = getInputText.toLowerCase(); //add conteudo no input msg
+  console.log(msgEncript.value);
 };
-// letterCode();
-// let letterDecoding = () => {
-//   let inputText = document.getElementById("input-text").value;
-//   //recebe conteudo do input e convert as vogais
-//   let getInputText = inputText
-//     .replace(/enter/gi, "e")
-//     .replace(/imes/gi, "i")
-//     .replace(/ai/gi, "a")
-//     .replace(/ober/gi, "o")
-//     .replace(/ufat/gi, "u");
 
-//   let msgEncript = document.getElementById("msg");
-//   msgEncript.value = getInputText; //add conteudo no input msg
-
-//   return console.log(msgEncript.value);
-// };
+let cleaningInput = () => {
+  inputText.value = "";
+};
 
 /* Regras Codificador: 
 "e" é convertido para "enter" 
--- src/js/decodificador.js
+++ src/js/decodificador.js
@@ -1,3 +1,34 @@
+let buttonDecrypt = document.getElementById("button-decrypt");
+
+buttonDecrypt.addEventListener("click", (event) => {
+  event.preventDefault();
+  decryptLetters(); //descriptografa as letras
+  cleaningInput();
+});
+
+let decryptLetters = () => {
+  //recebe palavras chaves de criptografia
+  let keyWord = ["enter", "imes", "ai", "ober", "ufat"];
+  //verifica se existe alguma conjunto de caracateres na estring ou seja se esta criotigrafada
+  let checkWord = keyWord.some((word) => InputText.includes(word));
+  console.log(typeof checkWord);
+
+  if (checkWord) {
+    //recebe conteudo do input e convert as vogais
+    let getInputText = inputText.value
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+
+    msgEncript.value = getInputText.toLowerCase(); //add conteudo no input msg
+    console.log(msgEncript.value);
+  } else {
+    alert("Oi");
+  }
+};
+
 /* Regras Decodificador: 
 "enter" é convertido para "e" 
 "imes" é convertido para "i"
